2cde4e30094d305852b8da5c99342c834f035972
Ant-ropy <phil.roberts90@outlook.com>
2025-06-23 23:28:45 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,12 @@ import math
 import urllib.request
 import os
 
+from flask import Flask, jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app, origins=["https://www.antropy.net"])  # only allow your website
+
 
 # Video source
 VIDEO_URL = "https://filesamples.com/samples/video/mp4/sample_640x360.mp4"
